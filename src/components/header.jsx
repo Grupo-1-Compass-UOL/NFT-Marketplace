a7d9b00f65183bbddebe42237ecf494855fbdf04
LucasSantosOliveira2 <lucas-santos-2011@live.com>
2023-07-11 00:58:53 -0300

    feat: create header desktop

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,7 @@
 import { MenuBurguer } from './img/menuBurguer'
 import { LogoIcon } from './img/logoIcon'
+import { IconUser } from './img/iconUser'
+import './header.css';
 
 export function Header() {
     return (
@@ -9,7 +11,15 @@ export function Header() {
                 <img src="./nav-nav-storefront" alt="" />
                 <h5 className='logo-title'>NFT Marketplace</h5>
             </div>
-            <MenuBurguer />
-        </header>
+            <nav className='menu-header'>
+                <a href="">Marketplace</a>
+                <a href="">Rankings</a>
+                <a href="">Connect a wallet</a>
+                <button className='btn-menu'> <IconUser />Sign up</button>
+            </nav>
+            <div className='menu-burguer'>
+                <MenuBurguer />
+            </div>
+        </header >
     );
 }
