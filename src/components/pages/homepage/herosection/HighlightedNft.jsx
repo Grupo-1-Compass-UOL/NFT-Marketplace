e514a0529cd18743002fb51a2b7c68c3f879d8fa
Matheus Fagundes <matheusoliveira.aluno@unipampa.edu.br>
2023-07-12 17:21:55 -0300

    feat: NTF animation adicionada

--- src/components/pages/homepage/herosection/HighlightedNft.jsx
+++ src/components/pages/homepage/herosection/HighlightedNft.jsx
@@ -2,21 +2,9 @@ import styled from "./style/HeroSection.module.css";
 
 function HighlightedNft() {
   return (
-    <div className={styled.nft}>
-      <div className={styled.nftimag}>
-        <img
-          className="NFT"
-          src=".\src\assets\images\image-placeholder-homepage.png"
-          alt="NTF-homepage"
-        ></img>
-      </div>
-      <div className={styled.avatar}>
-        <p>Space Walking</p>
-        <img
-          className="avatar"
-          src=".\src\assets\images\avatar\artist-card.png"
-          alt="Avatar-Animakid"
-        ></img>
+    <div className={styled.box}>
+      <div className={styled.highlightedNFTwrapper}>
+        <div className={styled.highlightedNFT} />
       </div>
     </div>
   );
