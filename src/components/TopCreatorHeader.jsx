502401538b1376df17bf579b2b6b36750278b550
Auri Gabriel <ririmello.a@gmail.com>
2023-07-12 11:03:29 -0300

    feat: Update TopCreatorHeader and TopCreatorItem
    The commit includes updates to the TopCreatorHeader and TopCreatorItem
    components, improving their visual presentation and functionality.
    
    - In TopCreatorHeader.jsx:
      - Replaced the content of header__position with a "#" symbol.
    
    - In TopCreatorHeader.module.css:
      - Added color styling to the .header class.
      - Modified the styling for .header__position to display it as a
        centered 1.5rem by 1.5rem element.
      - Added media queries to display .header__change when the screen width
        is at least 768px, and to display .header__number_sold when the
        screen width is at least 1024px.
    
    - In TopCreatorItem.jsx:
      - Added a new div with the class .creator__card to wrap
        .creator__avatar and .creator__name.
    
    - In TopCreatorItem.module.css:
      - Added styling for the .creator__card class to display its child
        elements in a row with a gap of 1rem.
      - Modified the .creator__position styling to display it as a centered
        1.5rem by 1.5rem element.
      - Modified the .creator__name styling to set a maximum width of 10rem
        and occupy 100% of the available width.
      - Added media queries to display .creator__change when the screen
        width is at least 768px, and to display .creator__number_sold when
        the screen width is at least 1024px.
      - Added classes .positive__change and .negative__change to style
        positive and negative changes respectively.

--- src/components/TopCreatorHeader.jsx
+++ src/components/TopCreatorHeader.jsx
@@ -3,8 +3,7 @@ import styles from './TopCreatorHeader.module.css'
 const TopCreatorHeader = () => {
   return (
     <li className={styles.header}>
-      <div className={styles.header__position}>N</div>
-      <div className={styles.header__avatar}></div>
+      <div className={styles.header__position}>#</div>
       <div className={styles.header__name}>Artist</div>
       <div className={styles.header__change}>Change</div>
       <div className={styles.header__number_sold}>NFTs Sold</div>
--- src/components/TopCreatorItem.jsx
+++ src/components/TopCreatorItem.jsx
@@ -2,13 +2,15 @@ import PropTypes from "prop-types";
 import styles from "./TopCreatorItem.module.css";
 
 const TopCreatorItem = (props) => {
-  const { position, image, name, change, numberSold, volumeSold } = props;
+  const {position, image, name, change, numberSold, volumeSold} = props;
 
   return (
     <li className={styles.creator}>
       <div className={styles.creator__position}>{position}</div>
-      <div className={styles.creator__avatar}>{image}</div>
-      <div className={styles.creator__name}>{name}</div>
+      <div className={styles.creator__card}>
+        <div className={styles.creator__avatar}>{image}</div>
+        <div className={styles.creator__name}>{name}</div>
+      </div>
       <div className={styles.creator__change}>{change}</div>
       <div className={styles.creator__number_sold}>{numberSold}</div>
       <div className={styles.creator__volume_sold}>{volumeSold}</div>
