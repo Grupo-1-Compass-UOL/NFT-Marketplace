d15fdc856450db3b6c30dae33a9e516213867a7e
Matheus Fagundes <matheusoliveira.aluno@unipampa.edu.br>
2023-07-12 12:02:30 -0300

    feat: update first page of homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
 import HeroSection from './components/pages/homepage/herosection/HeroSection';
+import Footer from './components/footer/footer';
+
 import './App.css';
 
 function App() {
   return (
     <main>
+      
       <HeroSection />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,22 @@
+import { MenuBurguer } from '../img/menuBurguer'
+import { IconUser } from '../img/iconUser'
+import { Logo } from '../logo/logo'
+
+import headerStyles from './header.module.css';
+
+export function Header() {
+    return (
+        <header className={headerStyles.headerContainer}>
+            <Logo />
+            <nav className={headerStyles.menuHeader}>
+                <a>Marketplace</a>
+                <a href="">Rankings</a>
+                <a href="">Connect a wallet</a>
+                <button className={headerStyles.btnMenu}> <IconUser />Sign up</button>
+            </nav>
+            <div className={headerStyles.menuBurguer}>
+                <MenuBurguer />
+            </div>
+        </header >
+    );
+}
