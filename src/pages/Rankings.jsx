0ba0d7368ae7322eb371aab32f61d583d8cc0a6b
Auri Gabriel <ririmello.a@gmail.com>
2023-07-13 13:29:48 -0300

    feat: improvements

--- src/pages/Rankings.jsx
+++ src/pages/Rankings.jsx
@@ -2,52 +2,23 @@ import TabBar from '../components/TabBar/TabBar';
 import {TopCreatorList} from '../components/TopCreatorList';
 import styles from './Rankings.module.css';
 
-const creators = [
-  {
-    position: 1,
-    image: {
-      src: "",
-      alt: ""
-    },
-    name: "John Doe",
-    change: "+10%",
-    numberSold: 100,
-    volumeSold: "$1000"
-  },
-  {
-    position: 2,
+const creators = [];
+
+for (let i = 0; i < 20; i++) {
+  let obj = {
+    position: i + 1,
     image: {
       src: "",
       alt: ""
     },
     name: "Jane Smith",
-    change: "-5%",
-    numberSold: 80,
-    volumeSold: "$800"
-  },
-  {
-    position: 3,
-    image: {
-      src: "",
-      alt: ""
-    },
-    name: "Jane Smith",
-    change: "-5%",
-    numberSold: 80,
-    volumeSold: "$800"
-  },
-  {
-    position: 4,
-    image: {
-      src: "",
-      alt: ""
-    },
-    name: "Jane Smith",
-    change: "-5%",
-    numberSold: 80,
-    volumeSold: "$800"
-  },
-];
+    change: "+1.41%",
+    numberSold: 602,
+    volumeSold: "12.4 ETH"
+  };
+  creators.push(obj);
+}
+
 
 const Rankings = () => {
   return (
