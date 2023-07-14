7786df7264ab810aaec110c7bcbaced9b5a62189
Hevellyn Oshiro <oshirohevellyn@gmail.com>
2023-07-14 19:47:51 -0300

    fix, app.jsx, added const routes, and passing they like a parameter in function createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import './App.css';
 import Rankings from './pages/Rankings';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-import {RootLayout} from './pages/Root';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RootLayout from './pages/Root';
 
-const BrowserRouter = createBrowserRouter([
+const routes = [
   {
-    path: '/', element: <RootLayout/>, children: [
-      {path: '/', element: <Rankings />},
-      {path: '/rankings', element: <Rankings />},
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <Rankings /> },
+      { path: '/rankings', element: <Rankings /> },
     ],
   },
-]);
+];
 
+const BrowserRouter = createBrowserRouter(routes);
 
 function App() {
   return <RouterProvider router={BrowserRouter} />;
