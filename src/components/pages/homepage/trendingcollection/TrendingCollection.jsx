19fdf2ece0280f6ee17eef57bda477608b8fc8f1
Matheus Fagundes <matheusoliveira.aluno@unipampa.edu.br>
2023-07-15 12:59:25 -0300

    feat: first section completed, full version

--- src/components/pages/homepage/trendingcollection/TrendingCollection.jsx
+++ src/components/pages/homepage/trendingcollection/TrendingCollection.jsx
@@ -2,7 +2,26 @@
 
 function TrendingCollection() {
   return (
-    <div>TrendingCollection</div>
+    <div className="TrendingCollection">
+      <div className="sectionHeadline">
+        <h1>Trending Collection</h1>
+        <h4>Checkout our weekly updated trending collection.</h4>
+      </div>
+      <div className="CollectionSlider">
+        <div className="CollectionCard">
+          <div className="PhotosNumbers">
+            <img></img>
+          </div>
+          <div className="CollectionInfo">
+            <h4>DSGN Animals</h4>
+            <div className="ArtistCard">
+              <img></img>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
