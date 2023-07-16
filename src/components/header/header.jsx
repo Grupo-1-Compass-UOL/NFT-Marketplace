66d14496ab173bb022ed2d40716a5c4e0ec30092
Auri Gabriel <ririmello.a@gmail.com>
2023-07-16 20:07:49 -0300

    fix: filenames to pascal case

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,7 +2,7 @@ import { MenuBurguer } from '../../assets/images/img/menuBurguer'
 import { IconUser } from '../../assets/images/img/iconUser'
 import { Logo } from '../logo/logo'
 
-import Styles from './Header.module.css';
+import Styles from './header.module.css';
 
 function Header() {
     return (
