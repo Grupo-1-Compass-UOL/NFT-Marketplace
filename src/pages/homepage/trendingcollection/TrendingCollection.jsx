588f7cb5a793dc4ce5e63c2c0dbedc326961acdf
Matheus Fagundes <matheusoliveira.aluno@unipampa.edu.br>
2023-07-17 17:46:32 -0300

    feat: adcionado rotas

--- src/pages/homepage/trendingcollection/TrendingCollection.jsx
+++ src/pages/homepage/trendingcollection/TrendingCollection.jsx
@@ -1,7 +1,6 @@
 import SectionHeadline from "./SectionHeadline";
 import style from "./style/TrendingCollection.module.css";
 import { Link } from "react-router-dom";
-
 function TrendingCollection() {
   return (
     <Link to="/NFTPage">
