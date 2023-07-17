c970b62b45b798341128404aea8b23ef74eff38d
LucasSantosOliveira2 <lucas-santos-2011@live.com>
2023-07-17 02:33:24 -0300

    Committing local changes before reverting merge commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
 import './App.css';
-<<<<<<< HEAD
-import { ConnectWallet } from './components/ConnectWallet/ConnectWallet';
-import { CreateAccount } from './components/CreateAccount/CreateAccount';
-import Footer from './components/Footer/Footer';
-import SubscribeWidget from './components/pages/homepage/SubscribeWidget/SubscribeWidget';
-function App() {
-  return (
-    <main>
-<<<<<<< HEAD
-
-=======
-      <Header />
-      <HeroSection />
-      <TrendingCollection />
-      <Footer />
-  
->>>>>>> eb8ffe7f60330511d48962101857de9bd6d4e9c2
-    </main >
-  )
-
-}
-
-export default App;
-
-
-=======
 import Rankings from './pages/Rankings';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './pages/Root';
 import NFTPage from './components/NFT-Page/NFTPage';
+import { CreateAccount } from './components/CreateAccount/CreateAccount';
 
 const routes = [
   {
@@ -37,15 +12,16 @@ const routes = [
     children: [
       { path: '/', element: <Rankings /> },
       { path: '/rankings', element: <Rankings /> },
-      { path: '/NFTPage', element: <NFTPage />},
+      { path: '/NFTPage', element: <NFTPage /> },
     ],
   },
 ];
 const BrowserRouter = createBrowserRouter(routes);
 
 function App() {
-  return <RouterProvider router={BrowserRouter} />;
+  return {
+      < CreateAccount />
+  };
 }
 
 export default App;
->>>>>>> 793e16af10d0ff4fa7b71eabe0db4501cfbd162b
