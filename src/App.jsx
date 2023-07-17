0d6fb9476796634790bd183448333ed899b30900
LucasSantosOliveira2 <lucas-santos-2011@live.com>
2023-07-17 02:58:23 -0300

    recuperando

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import './App.css';
-import { ConnectWallet } from './components/ConnectWallet/ConnectWallet';
-import { CreateAccount } from './components/CreateAccount/CreateAccount';
-import Footer from './components/Footer/Footer';
-import SubscribeWidget from './components/pages/homepage/SubscribeWidget/SubscribeWidget';
+import Rankings from './pages/Rankings';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RootLayout from './pages/Root';
+import NFTPage from './components/NFT-Page/NFTPage';
+
+const routes = [
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <Rankings /> },
+      { path: '/rankings', element: <Rankings /> },
+      { path: '/NFTPage', element: <NFTPage /> },
+    ],
+  },
+];
+const BrowserRouter = createBrowserRouter(routes);
+
 function App() {
-  return (
-    <main>
-<<<<<<< HEAD
-
-=======
-      <Header />
-      <HeroSection />
-      <TrendingCollection />
-      <Footer />
-  
->>>>>>> eb8ffe7f60330511d48962101857de9bd6d4e9c2
-    </main >
-  )
-
+  return <RouterProvider router={BrowserRouter} />;
 }
 
 export default App;
