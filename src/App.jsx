c20c6850b25e9726777c25643217792a089cdd05
Hevellyn Oshiro <oshirohevellyn@gmail.com>
2023-07-17 12:21:29 -0300

    fix: Link Button

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const routes = [
       { path: '/', element: <Homepage/> },
       { path: '/rankings', element: <Rankings /> },
       { path: '/NFTPage', element: <NFTPage />},
+    
     ],
   },
 ];
--- src/pages/homepage/topratedartists/Button.jsx
+++ src/pages/homepage/topratedartists/Button.jsx
@@ -1,13 +1,15 @@
 import RocketLaunchNevativo from "../../../assets/images/img/RocketLaunchNegativo";
 import styled from "./style/TopRatedArtists.module.css";
-
+import { Link } from "react-router-dom"
 function Button() {
   return (
     <div className={styled.button}>
-      <button>
-          <RocketLaunchNevativo />
-          View Rankings
-      </button>
+      <Link to="/rankings">
+        <button>
+            <RocketLaunchNevativo />
+            View Rankings
+        </button>
+      </Link>
     </div>
   );
 }
