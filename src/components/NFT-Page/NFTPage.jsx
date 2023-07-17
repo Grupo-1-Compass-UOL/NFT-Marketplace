d8494175e345a549d71826a6b85d581fa24bf03b
Hevellyn Oshiro <oshirohevellyn@gmail.com>
2023-07-17 00:38:58 -0300

    feat: added routes on Card to NFTPage

--- src/components/NFT-Page/NFTPage.jsx
+++ src/components/NFT-Page/NFTPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import classes from './nft_page.module.css'
 import ArtistInfo from './ArtistInfo/ArtistInfo'
-import { Header } from '../header'
 import ImagePlaceHolder from './ImagePlaceHolder/ImagePlaceHolder'
 import MoreNFTS from './MoreNfts/MoreNFTS'
 
@@ -11,16 +10,13 @@ const NFTPage = () => {
   return (
     <div className={classes.NFTPagedesktop}>
           <header className={classes.header}>
-            <Header/>
             <ImagePlaceHolder/>
           </header>
           <main className={classes.main}>
             <ArtistInfo/>
             <MoreNFTS/>
           </main>
-          <footer>
-
-          </footer>
+   
         </div>
 )
 }
