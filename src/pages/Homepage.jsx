1600e56789bbfeaaf2f394604686c12629c505ea
Matheus Fagundes <matheusoliveira.aluno@unipampa.edu.br>
2023-07-17 12:52:53 -0300

    feat: juntando Subscribe ao newDevelop

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,6 +3,7 @@ import HeroSection from './homepage/herosection/HeroSection';
 import TrendingCollection from './homepage/trendingcollection/TrendingCollection';
 import TopRatedArtists from './homepage/topratedartists/TopRatedArtists';
 import BrowseCategories from './homepage/BrowseCategories/BrowseCategories';
+import SubscribeWidget from './homepage/SubscribeWidget/SubscribeWidget';
 
 
 function Homepage() {
@@ -12,6 +13,7 @@ function Homepage() {
       <TrendingCollection />
       <TopRatedArtists />
       <BrowseCategories />
+      <SubscribeWidget />
     </>
   )
 }
