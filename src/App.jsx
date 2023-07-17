586e0ea16fc236a41047fcec1144457b6123a1a9
LucasSantosOliveira2 <lucas-santos-2011@live.com>
2023-07-17 14:34:19 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Rankings from './pages/Rankings';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './pages/Root';
 import NFTPage from './components/NFT-Page/NFTPage';
+import Homepage from './pages/Homepage';
 
 const routes = [
   {
     path: '/',
     element: <RootLayout />,
     children: [
-      { path: '/', element: <Rankings /> },
+      { path: '/', element: <Homepage /> },
       { path: '/rankings', element: <Rankings /> },
-      { path: '/NFTPage', element: <NFTPage />},
+      { path: '/NFTPage', element: <NFTPage /> },
+
     ],
   },
 ];
